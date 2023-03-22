92c1810c4ac58e2e18691133eafec34d7ea0f445
Fangming Ning <synfm@126.com>
2023-03-22 18:51:30 -0500

    Update target-checkout.js

--- target-checkout.js
+++ target-checkout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Target.com Checkout Bot
 // @namespace    https://github.com/DerekFangming/tampermonkey-scripts
-// @version      1.0
+// @version      1.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.target.com/*
